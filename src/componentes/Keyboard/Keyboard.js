22e5c1f371c0b9e0e6e4b671af963848dcecf1ad
Rodica Fazakas <11833084+rodicafazakas@users.noreply.github.com>
2021-10-21 00:28:24 +0200

    Added all functionalities

--- src/componentes/Keyboard/Keyboard.js
+++ src/componentes/Keyboard/Keyboard.js
@@ -1,10 +1,13 @@
-const Keyboard = ({actionOnClick}) => {
+const Keyboard = ({actionOnClick, disabledState}) => {
     const teclas = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "DELETE"];
 
     return (
         <ol className="keyboard">
             {
-                teclas.map( (tecla) => <button key={tecla} className="key" value={tecla} onClick={actionOnClick}> {tecla} </button>) 
+                teclas.map( (tecla) => <button key={tecla} className="key" 
+                                               value={tecla} 
+                                               onClick={actionOnClick}
+                                               disabled={disabledState}> {tecla} </button>) 
             }
         </ol>
     )
